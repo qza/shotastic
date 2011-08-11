1d2102b1fc81798b0115315a37e5f84456038148
Zoran Kokovic <kokovic.zoran@gmail.com>
2011-08-11 15:12:45 +0200

    Main shooter setup and test success.

--- shooter.rb
+++ shooter.rb
@@ -1,3 +1,13 @@
+require "yaml"
+
 module Shooter
+  
+  def bootup_shooter
+    @shot_prefix = YAML.load_file('../config.yaml')['config']['shot_prefix']
+  end
+  
+  def current_shot_prefix
+    @shot_prefix
+  end
   
 end
--- test/test_basic.rb
+++ test/test_basic.rb
@@ -4,17 +4,17 @@ require "./test_helper"
 
 class TestBasics < Test::Unit::TestCase
   def test_configuration_presence
-    script_name = TestHelper.get_config
+    script_name = TestHelper.get_script_name
     assert(!script_name.nil?, "Configure main project script")
   end
 
   def test_script_exist
-    script_name = TestHelper.get_config
+    script_name = TestHelper.get_script_name
     assert(File.exists?("../#{script_name}"), "Create main project cript #{script_name}")
   end
 
   def test_module_defined
-    script_name = TestHelper.get_config
+    script_name = TestHelper.get_script_name
     require "../#{script_name}"
     mod_name = script_name.split(".")[0].capitalize
     @mod = Object.const_defined? mod_name
