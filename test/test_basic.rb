52002ba3e16e406cb2b09936c7f8a7f420a5ba3e
Zoran Kokovic <kokovic.zoran@gmail.com>
2011-08-11 08:41:37 +0200

    Testing for main script existance. Failed.

--- test/test_basic.rb
+++ test/test_basic.rb
@@ -7,4 +7,10 @@ class TestBasics < Test::Unit::TestCase
     script_name = YAML.load_file('./config.yml')['config']['script_name']
     assert(!script_name.nil?, "Configure main project script")
   end
+  
+  def test_script_exist
+    script_name = YAML.load_file('./config.yml')['config']['script_name']
+    assert(File.exists?(script_name), "Create main project cript #{script_name}")
+  end
+  
 end
