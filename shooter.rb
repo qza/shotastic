a34c64a14a6e984b3b5c8cdde6d6d7d1dba6c864
Zoran <kokovic.zoran@gmail.com>
2011-08-12 00:08:10 +0200

    Basic capturing and thumbnail generation

--- shooter.rb
+++ shooter.rb
@@ -1,13 +1,29 @@
-require "yaml"
+require "./config_helper"
 
 module Shooter
-  
+
+  attr_accessor :images
   def bootup_shooter
-    @shot_prefix = YAML.load_file('../config.yaml')['config']['shot_prefix']
+    @images = []
   end
-  
-  def current_shot_prefix
-    @shot_prefix
+
+  def capture(id='root', quality='75')
+    img = Image.capture {
+      self.filename = id
+      self.quality = quality
+    }
+    @images < img
   end
-  
+
+  def persist(thumb=false)
+    @i = Dir["*.jpg"].length()
+    @images.each_with_index do |e, i|
+      t = e.minify
+      e.write "#{shot_prefix}_#{@i}.jpg"
+      if thumb?
+        e.minify.write "#{shot_prefix}_thumb_#{@i}.jpg"
+      end
+    end
+  end
+
 end
