29c472209f731b8c8db44b7211f8f758f0c537d9
Zoran Kokovic <kokovic.zoran@gmail.com>
2011-08-11 08:39:00 +0200

    Created main project configuration file. Created test for configuration. Failed.

--- test/test_basic.rb
+++ test/test_basic.rb
@@ -1,5 +1,10 @@
 require "test/unit"
+require "yaml"
 
 class TestBasics < Test::Unit::TestCase
 
+  def test_configuration_presence
+    script_name = YAML.load_file('./config.yml')['config']['script_name']
+    assert(!script_name.nil?, "Configure main project script")
+  end
 end
